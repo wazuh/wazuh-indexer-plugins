5b49a46707b78c4d61bc8c3907b458e4410939cd
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-07-02 19:53:56 +0200

    Fix forbidden API errors

--- plugins/wazuh-indexer-setup/src/main/java/org/wazuh/setup/index/WazuhIndices.java
+++ plugins/wazuh-indexer-setup/src/main/java/org/wazuh/setup/index/WazuhIndices.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
+import java.util.Locale;
 
 public class WazuhIndices {
 
@@ -36,30 +37,49 @@ public class WazuhIndices {
     private static String INDEX_MAPPING_FILE_NAME = "index-mapping.yml";
     private static String INDEX_SETTING_FILE_NAME = "index-settings.yml";
 
+    /**
+     * Constructor
+     * @param client Client
+     * @param clusterService ClusterService
+     */
     public WazuhIndices(Client client, ClusterService clusterService) {
         this.client = client;
         this.clusterService = clusterService;
     }
 
+    /**
+     *
+     * @return string
+     */
     public String getIndexMapping() {
         try (InputStream is = getClass().getClassLoader().getResourceAsStream(INDEX_MAPPING_FILE_NAME)) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             Streams.copy(is, out);
             return out.toString(StandardCharsets.UTF_8);
         } catch (Exception e) {
-            String errorMessage = MessageFormat.format("failed to load index mapping file [{0}]", INDEX_MAPPING_FILE_NAME);
+            String errorMessage = new MessageFormat(
+                "failed to load index mapping file [{0}]",
+                Locale.ROOT
+            ).format(INDEX_MAPPING_FILE_NAME);
             log.error(errorMessage, e);
             throw new IllegalStateException(errorMessage, e);
         }
     }
 
+    /**
+     *
+     * @return string
+     */
     public String getIndexSettings() {
         try (InputStream is = getClass().getClassLoader().getResourceAsStream(INDEX_SETTING_FILE_NAME)) {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             Streams.copy(is, out);
             return out.toString(StandardCharsets.UTF_8);
         } catch (Exception e) {
-            String errorMessage = MessageFormat.format("failed to load index settings file [{0}]", INDEX_SETTING_FILE_NAME);
+            String errorMessage = new MessageFormat(
+                "failed to load index settings file [{0}]",
+                Locale.ROOT
+            ).format(INDEX_SETTING_FILE_NAME);
             log.error(errorMessage, e);
             throw new IllegalStateException(errorMessage, e);
         }
