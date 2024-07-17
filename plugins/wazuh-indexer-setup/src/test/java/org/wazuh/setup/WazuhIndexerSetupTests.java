5869fbbeb6885abb6255ec1d02dfd346638daf2d
Fede Tux <fede@fernetcave.com>
2024-07-17 16:12:32 -0300

    Adding tests

--- plugins/wazuh-indexer-setup/src/test/java/org/wazuh/setup/WazuhIndexerSetupTests.java
+++ plugins/wazuh-indexer-setup/src/test/java/org/wazuh/setup/WazuhIndexerSetupTests.java
@@ -5,10 +5,55 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
-package org.wazuh.setup;
+package org.wazuh.setup.index;
 
+import org.junit.Before;
 import org.opensearch.test.OpenSearchTestCase;
+import org.opensearch.client.Client;
+import org.opensearch.cluster.service.ClusterService;
 
 public class WazuhIndexerSetupTests extends OpenSearchTestCase {
-    // Add unit tests for your plugin
+
+private WazuhIndices wazuhIndices;
+private Client client;
+private ClusterService clusterService;
+
+  @Before
+  public void setUp() throws Exception {
+    //public static final String INDEX_NAME = "wazuh-indexer-setup-plugin";
+    //private static final String INDEX_MAPPING_FILE_NAME = "index-mapping.yml";
+    //private static final String INDEX_SETTING_FILE_NAME = "index-settings.yml";
+    super.setUp();
+    this.wazuhIndices = new WazuhIndices(client, clusterService);
+  }
+
+  public void testGetIndexMapping() {
+    //String indexMapping = getIndexMapping();
+    //InputStream is = getClass().getClassLoader().getResourceAsStream(INDEX_MAPPING_FILE_NAME)
+    //ByteArrayOutputStream out = new ByteArrayOutputStream();
+    //Streams.copy(is, out);
+    //return out.toString(StandardCharsets.UTF_8);
+    assertEquals(0,0);
+  }
+
+  public void testGetIndexSettings() {
+    assertEquals(0,0);
+
+  }
+
+  public void testPutTemplate() {
+    assertEquals(0,0);
+
+  }
+
+  public void testCreate() {
+    assertEquals(0,0);
+
+  }
+
+  public void testIndexExists() {
+    assertEquals(0,0);
+
+  }
+
 }
