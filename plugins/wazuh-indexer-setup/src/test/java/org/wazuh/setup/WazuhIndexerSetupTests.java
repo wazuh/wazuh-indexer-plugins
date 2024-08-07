c0d9aba01108b46c5c431f2d3fddb92c30337c20
Fede Tux <fede@fernetcave.com>
2024-08-07 17:10:16 -0300

    Adding unit tests for putTemplate, create and indexExists methods

--- plugins/wazuh-indexer-setup/src/test/java/org/wazuh/setup/WazuhIndexerSetupTests.java
+++ plugins/wazuh-indexer-setup/src/test/java/org/wazuh/setup/WazuhIndexerSetupTests.java
@@ -13,10 +13,15 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.opensearch.action.admin.indices.create.CreateIndexRequest;
+import org.opensearch.action.admin.indices.create.CreateIndexResponse;
+import org.opensearch.action.admin.indices.template.put.PutIndexTemplateRequest;
 import org.opensearch.action.support.master.AcknowledgedResponse;
 import org.opensearch.client.AdminClient;
 import org.opensearch.client.Client;
 import org.opensearch.client.IndicesAdminClient;
+import org.opensearch.cluster.routing.RoutingTable;
 import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.util.io.Streams;
 import org.opensearch.core.action.ActionListener;
@@ -34,64 +39,134 @@ public class WazuhIndexerSetupTests extends OpenSearchTestCase {
   private ClusterService clusterService;
   private static final String INDEX_MAPPING_FILE_NAME = "index-mapping.yml";
   private static final String INDEX_SETTING_FILE_NAME = "index-settings.yml";
+  private Client mockClient;
+
 
   @Before
   public void setUp() throws Exception {
     super.setUp();
 
-    threadPool = new TestThreadPool("WazuhIndexerSetupPluginServiceTests");
-    clusterService = createClusterService(threadPool);
-
-
-    Client mockClient = mock(Client.class);
-    AdminClient mockAdminClient = mock(AdminClient.class);
-    IndicesAdminClient mockIndicesAdminClient = mock(IndicesAdminClient.class);
-    when(mockClient.admin()).thenReturn(mockAdminClient);
-    when(mockAdminClient.indices()).thenReturn(mockIndicesAdminClient);
-
-
+    this.threadPool = new TestThreadPool("WazuhIndexerSetupPluginServiceTests");
+    this.clusterService = createClusterService(threadPool);
+    this.mockClient = mock(Client.class);
     this.wazuhIndices = new WazuhIndices(mockClient, clusterService);
-
   }
 
   @After
   public void testTearDown() throws Exception {
-    threadPool.shutdownNow();
-    clusterService.close();
+    this.threadPool.shutdownNow();
+    this.clusterService.close();
   }
 
+  @Test
   public void testGetIndexMapping() throws IOException {
-    String indexMapping = wazuhIndices.getIndexMapping();
+    String indexMapping = this.wazuhIndices.getIndexMapping();
     InputStream is = WazuhIndices.class.getClassLoader().getResourceAsStream(INDEX_MAPPING_FILE_NAME);
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     Streams.copy(is, out);
-    assertEquals(out.toString(StandardCharsets.UTF_8),wazuhIndices.getIndexMapping());
+    assertEquals(out.toString(StandardCharsets.UTF_8),this.wazuhIndices.getIndexMapping());
   }
 
+  @Test
   public void testGetIndexSettings() throws IOException {
     InputStream is = getClass().getClassLoader().getResourceAsStream(INDEX_SETTING_FILE_NAME);
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     Streams.copy(is, out);
-    assertEquals(out.toString(StandardCharsets.UTF_8), wazuhIndices.getIndexSettings());
+    assertEquals(out.toString(StandardCharsets.UTF_8), this.wazuhIndices.getIndexSettings());
   }
 
+  @Test
   public void testPutTemplate() {
-    ActionListener<AcknowledgedResponse> actionListener = mock(ActionListener.class);
+
+    AdminClient mockAdminClient = mock(AdminClient.class);
+    IndicesAdminClient mockIndicesAdminClient = mock(IndicesAdminClient.class);
+    when(this.mockClient.admin()).thenReturn(mockAdminClient);
+    when(mockAdminClient.indices()).thenReturn(mockIndicesAdminClient);
+
     doAnswer( invocation -> {
-      AcknowledgedResponse response = invocation.getArgument(0);
-      logger.error(response);
-      assertTrue(response.isAcknowledged());
+      ActionListener<AcknowledgedResponse> listener = invocation.getArgument(1);
+      listener.onResponse(new AcknowledgedResponse(true));
       return null;
-    }).when(actionListener).onResponse(any(AcknowledgedResponse.class));
+    }).when(mockIndicesAdminClient).putTemplate(any(PutIndexTemplateRequest.class), any(ActionListener.class));
+
+    ActionListener<AcknowledgedResponse> actionListener = new ActionListener<AcknowledgedResponse>() {
+      @Override
+      public void onResponse(AcknowledgedResponse acknowledgedResponse) {
+        assertTrue(acknowledgedResponse.isAcknowledged());
+      }
+      @Override
+      public void onFailure(Exception e) {
+        logger.error("Mock error putting template: {}", e.toString());
+      }
+    };
+
+    this.wazuhIndices.putTemplate(actionListener);
+
+    doAnswer( invocation -> {
+      ActionListener<AcknowledgedResponse> listener = invocation.getArgument(1);
+      listener.onFailure(new Exception("Mock exception on putTemplate"));
+      return null;
+    }).when(mockIndicesAdminClient).putTemplate(any(PutIndexTemplateRequest.class), any(ActionListener.class));
+
     this.wazuhIndices.putTemplate(actionListener);
   }
 
+  @Test
   public void testCreate() {
-    assertEquals(0,0);
+    AdminClient mockAdminClient = mock(AdminClient.class);
+    IndicesAdminClient mockIndicesAdminClient = mock(IndicesAdminClient.class);
+    when(this.mockClient.admin()).thenReturn(mockAdminClient);
+    when(mockAdminClient.indices()).thenReturn(mockIndicesAdminClient);
 
+    doAnswer( invocation -> {
+      ActionListener<CreateIndexResponse> listener = invocation.getArgument(1);
+      listener.onResponse(new CreateIndexResponse(true,true, WazuhIndices.INDEX_NAME));
+      return null;
+    }).when(mockIndicesAdminClient).create(any(CreateIndexRequest.class), any(ActionListener.class));
+
+    ActionListener<CreateIndexResponse> actionListener = new ActionListener<>() {
+      @Override
+      public void onResponse(CreateIndexResponse createIndexResponse) {
+        logger.info("Mock successful index creation");
+        assertTrue(createIndexResponse.isAcknowledged());
+      }
+
+      @Override
+      public void onFailure(Exception e) {
+        logger.error("Mock error creating index: {}", e.toString());
+      }
+    };
+
+    try {
+      this.wazuhIndices.create(actionListener);
+    }
+    catch (Exception e) {
+      logger.error(e);
+    }
+
+    doAnswer( invocation -> {
+      ActionListener<CreateIndexResponse> listener = invocation.getArgument(1);
+      listener.onFailure(new Exception("Mock Exception"));
+      return null;
+    }).when(mockIndicesAdminClient).create(any(CreateIndexRequest.class), any(ActionListener.class));
+
+    try {
+      this.wazuhIndices.create(actionListener);
+    }
+    catch (Exception e) {
+      logger.error(e);
+    }
   }
 
+  @Test
   public void testIndexExists() {
-    assertEquals(0,0);
+    RoutingTable spyRoutingTable = spy(this.clusterService.state().getRoutingTable());
+
+    when(spyRoutingTable.hasIndex(WazuhIndices.INDEX_NAME)).thenReturn(true);
+    logger.error(this.wazuhIndices.indexExists(WazuhIndices.INDEX_NAME));
+
+    when(spyRoutingTable.hasIndex(WazuhIndices.INDEX_NAME)).thenReturn(false);
+    logger.error(this.wazuhIndices.indexExists(WazuhIndices.INDEX_NAME));
+
   }
 }
