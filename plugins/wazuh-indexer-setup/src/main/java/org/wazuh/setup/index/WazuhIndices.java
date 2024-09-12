e71947c1f2791fddd8f0bc480fff4b9385dd7164
f-galland <federico.galland@wazuh.com>
2024-09-12 18:19:52 -0300

    Create wazuh-states-inventory-vulnerabilities index template

--- plugins/wazuh-indexer-setup/src/main/java/org/wazuh/setup/index/WazuhIndices.java
+++ plugins/wazuh-indexer-setup/src/main/java/org/wazuh/setup/index/WazuhIndices.java
@@ -51,6 +51,7 @@ public class WazuhIndices {
         // Create Index Templates - Indices map
         this.indexTemplates.put("index-template-agent", ".agents");
         this.indexTemplates.put("index-template-alerts", "wazuh-alerts-5.x-0001");
+        this.indexTemplates.put("index-template-vulnerabilities", "wazuh-states-inventory-vulnerabilities");
     }
 
     /**
