6e9756078207ac99535c87903b1a3342c2c5dfdc
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-09-17 13:55:54 +0200

    Add PoC extending the Job-Scheduler plugin
    Not functional yet

---
+++ plugins/command-manager/src/main/java/com/wazuh/commandmanager/CommandManagerJobParameter.java
@@ -0,0 +1,128 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+package com.wazuh.commandmanager;
+
+import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.jobscheduler.spi.ScheduledJobParameter;
+import org.opensearch.jobscheduler.spi.schedule.Schedule;
+
+import java.io.IOException;
+import java.time.Instant;
+
+/**
+ * A sample job parameter.
+ * <p>
+ * It adds "indexToWatch" field to {@link ScheduledJobParameter}, which stores the index
+ * the job runner will watch.
+ */
+public class CommandManagerJobParameter implements ScheduledJobParameter {
+    public static final String NAME_FIELD = "name";
+    public static final String ENABLED_FILED = "enabled";
+    public static final String LAST_UPDATE_TIME_FIELD = "last_update_time";
+    public static final String LAST_UPDATE_TIME_FIELD_READABLE = "last_update_time_field";
+    public static final String SCHEDULE_FIELD = "schedule";
+    public static final String ENABLED_TIME_FILED = "enabled_time";
+    public static final String ENABLED_TIME_FILED_READABLE = "enabled_time_field";
+    public static final String INDEX_NAME_FIELD = "index_name_to_watch";
+
+    private String jobName;
+    private Instant lastUpdateTime;
+    private Instant enabledTime;
+    private boolean isEnabled;
+    private Schedule schedule;
+    private String indexToWatch;
+
+    public CommandManagerJobParameter() {}
+
+    public CommandManagerJobParameter(String name, String indexToWatch, Schedule schedule) {
+        this.jobName = name;
+        this.indexToWatch = indexToWatch;
+        this.schedule = schedule;
+
+        Instant now = Instant.now();
+        this.isEnabled = true;
+        this.enabledTime = now;
+        this.lastUpdateTime = now;
+    }
+
+
+    @Override
+    public String getName() {
+        return this.jobName;
+    }
+
+
+    @Override
+    public Instant getLastUpdateTime() {
+        return this.lastUpdateTime;
+    }
+
+
+    @Override
+    public Instant getEnabledTime() {
+        return this.enabledTime;
+    }
+
+
+    @Override
+    public Schedule getSchedule() {
+        return this.schedule;
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+
+    public String getIndexToWatch() {
+        return indexToWatch;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public void setLastUpdateTime(Instant lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public void setEnabledTime(Instant enabledTime) {
+        this.enabledTime = enabledTime;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setIndexToWatch(String indexToWatch) {
+        this.indexToWatch = indexToWatch;
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.startObject();
+        builder.field(NAME_FIELD, this.jobName);
+        builder.field(ENABLED_FILED, this.isEnabled);
+        builder.field(SCHEDULE_FIELD, this.schedule);
+        builder.field(INDEX_NAME_FIELD, this.indexToWatch);
+        if (this.enabledTime != null) {
+            builder.timeField(ENABLED_TIME_FILED, ENABLED_TIME_FILED_READABLE, this.enabledTime.toEpochMilli());
+        }
+        if (this.lastUpdateTime != null) {
+            builder.timeField(LAST_UPDATE_TIME_FIELD, LAST_UPDATE_TIME_FIELD_READABLE, this.lastUpdateTime.toEpochMilli());
+        }
+        builder.endObject();
+
+        return builder;
+    }
+}
