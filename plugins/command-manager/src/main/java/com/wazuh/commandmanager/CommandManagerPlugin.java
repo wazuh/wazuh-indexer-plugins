f8ff61d6f5b69d7a886660aa255975b592cd1fff
f-galland <federico.galland@wazuh.com>
2024-09-18 18:31:06 -0300

    Adding POST endpoint

--- plugins/command-manager/src/main/java/com/wazuh/commandmanager/CommandManagerPlugin.java
+++ plugins/command-manager/src/main/java/com/wazuh/commandmanager/CommandManagerPlugin.java
@@ -7,9 +7,40 @@
  */
 package com.wazuh.commandmanager;
 
+import com.google.common.collect.ImmutableList;
+import com.wazuh.commandmanager.rest.action.RestPostCommandAction;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.node.DiscoveryNodes;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.IndexScopedSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.settings.SettingsFilter;
+import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.rest.RestController;
+import org.opensearch.rest.RestHandler;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 
-public class CommandManagerPlugin extends Plugin {
-    // Implement the relevant Plugin Interfaces here
+public class CommandManagerPlugin extends Plugin implements ActionPlugin {
+  public static final String COMMAND_MANAGER_BASE_URI = "/_plugins/_commandmanager";
+  public static final String COMMAND_MANAGER_INDEX_NAME = "command-manager";
+  private static final Logger log = LogManager.getLogger(CommandManagerPlugin.class);
+
+  public List<RestHandler> getRestHandlers(
+      Settings settings,
+      RestController restController,
+      ClusterSettings clusterSettings,
+      IndexScopedSettings indexScopedSettings,
+      SettingsFilter settingsFilter,
+      IndexNameExpressionResolver indexNameExpressionResolver,
+      Supplier<DiscoveryNodes> nodesInCluster
+  ) {
+    RestPostCommandAction restPostCommandAction = new RestPostCommandAction();
+    return ImmutableList.of(restPostCommandAction);
+  }
 }
