54262d992afefcc3fbe5fffeca46c9d16f9f6c00
f-galland <federico.galland@wazuh.com>
2024-10-02 09:50:42 -0300

    Remove index name

--- plugins/httprequests/src/main/java/com/wazuh/httprequests/HttpRequestsPlugin.java
+++ plugins/httprequests/src/main/java/com/wazuh/httprequests/HttpRequestsPlugin.java
@@ -8,29 +8,16 @@
 package com.wazuh.httprequests;
 
 import org.apache.hc.core5.http.HttpHost;
-import org.opensearch.client.Client;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.node.DiscoveryNodes;
-import org.opensearch.cluster.service.ClusterService;
 import org.opensearch.common.settings.ClusterSettings;
 import org.opensearch.common.settings.IndexScopedSettings;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.settings.SettingsFilter;
-import org.opensearch.core.common.io.stream.NamedWriteableRegistry;
-import org.opensearch.core.xcontent.NamedXContentRegistry;
-import org.opensearch.env.Environment;
-import org.opensearch.env.NodeEnvironment;
 import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
-import org.opensearch.repositories.RepositoriesService;
 import org.opensearch.rest.RestController;
 import org.opensearch.rest.RestHandler;
-import org.opensearch.script.ScriptService;
-import org.opensearch.threadpool.ThreadPool;
-import org.opensearch.watcher.ResourceWatcherService;
-
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -38,29 +25,9 @@ import static java.util.Collections.singletonList;
 
 
 public class HttpRequestsPlugin extends Plugin implements ActionPlugin {
-    static final String INDEX_NAME = "httprequests";
     static final String BASE_URI = "/_plugins/httprequests";
     static final HttpHost TARGET = new HttpHost("jsonplaceholder.typicode.com");
     static final String REQUEST_URI = "/todos/1";
-    ThreadPool threadPool;
-
-    @Override
-    public Collection<Object> createComponents(
-        Client client,
-        ClusterService clusterService,
-        ThreadPool threadPool,
-        ResourceWatcherService resourceWatcherService,
-        ScriptService scriptService,
-        NamedXContentRegistry xContentRegistry,
-        Environment environment,
-        NodeEnvironment nodeEnvironment,
-        NamedWriteableRegistry namedWriteableRegistry,
-        IndexNameExpressionResolver indexNameExpressionResolver,
-        Supplier<RepositoriesService> repositoriesServiceSupplier
-    ) {
-        this.threadPool = threadPool;
-        return Collections.emptyList();
-    }
 
     @Override
     public List<RestHandler> getRestHandlers(final Settings settings,
