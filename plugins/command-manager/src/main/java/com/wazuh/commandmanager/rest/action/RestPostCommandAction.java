8476ee48c68d607e8756e284785540457376395c
Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>
2024-10-15 23:38:05 +0200

    Test cycle
    Sends the command to a fake Management API on incoming POST requests for the command manager plugin.

--- plugins/command-manager/src/main/java/com/wazuh/commandmanager/rest/action/RestPostCommandAction.java
+++ plugins/command-manager/src/main/java/com/wazuh/commandmanager/rest/action/RestPostCommandAction.java
@@ -10,10 +10,15 @@ package com.wazuh.commandmanager.rest.action;
 import com.wazuh.commandmanager.CommandManagerPlugin;
 import com.wazuh.commandmanager.index.CommandIndex;
 import com.wazuh.commandmanager.model.Document;
+import com.wazuh.commandmanager.utils.httpclient.HttpRestClient;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.core5.net.URIBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.client.node.NodeClient;
+import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.rest.RestStatus;
+import org.opensearch.core.xcontent.ToXContent;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.rest.BaseRestHandler;
@@ -21,6 +26,8 @@ import org.opensearch.rest.BytesRestResponse;
 import org.opensearch.rest.RestRequest;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -36,7 +43,7 @@ import static org.opensearch.rest.RestRequest.Method.POST;
 public class RestPostCommandAction extends BaseRestHandler {
 
     public static final String POST_COMMAND_ACTION_REQUEST_DETAILS = "post_command_action_request_details";
-    private static final Logger logger = LogManager.getLogger(RestPostCommandAction.class);
+    private static final Logger log = LogManager.getLogger(RestPostCommandAction.class);
     private final CommandIndex commandIndex;
 
     /**
@@ -89,11 +96,36 @@ public class RestPostCommandAction extends BaseRestHandler {
      * @throws IOException thrown by the XContentParser methods.
      */
     private RestChannelConsumer handlePost(RestRequest request) throws IOException {
+        log.info(
+                "Received {} {} request id:{} from host:{}",
+                request.method().name(),
+                request.uri(),
+                request.getRequestId(),
+                request.header("Host")
+        );
         // Get request details
         XContentParser parser = request.contentParser();
         ensureExpectedToken(XContentParser.Token.START_OBJECT, parser.nextToken(), parser);
 
         Document document = Document.parse(parser);
+
+        // Commands delivery to the Management API.
+        // Note: needs to be decoupled from the Rest handler (job scheduler task).
+        HttpRestClient httpClient = HttpRestClient.getInstance();
+        try {
+            String uri = "https://httpbin.org/post";
+//            String uri = "https://127.0.0.1:5000";
+            URI receiverURI = new URIBuilder(uri)
+                    .build();
+            String payload = document.toXContent(XContentFactory.jsonBuilder(), ToXContent.EMPTY_PARAMS).toString();
+            SimpleHttpResponse response = httpClient.post(receiverURI, payload, document.getId());
+            log.info("Received response to POST request with code {}", response.getCode());
+            log.info("Raw response:\n{}", response.getBodyText());
+        } catch (URISyntaxException e) {
+            log.error("Bad URI:{}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Error reading response: {}", e.getMessage());
+        }
 
         // Send response
         return channel -> {
@@ -107,7 +139,7 @@ public class RestPostCommandAction extends BaseRestHandler {
                             builder.endObject();
                             channel.sendResponse(new BytesRestResponse(restStatus, builder));
                         } catch (Exception e) {
-                            logger.error("Error indexing command: ", e);
+                            log.error("Error indexing command: ", e);
                         }
                     }).exceptionally(e -> {
                         channel.sendResponse(new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
