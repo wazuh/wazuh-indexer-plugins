99ae457f34ef277761d7ae8397820ad1ee581500
Malena Casas <malena.casas@wazuh.com>
2024-10-25 14:37:18 -0300

    Add CommangManagerSettings and the respective tests (#120)
    * Add CommangManagerSettings and the respective tests
    
    * Apply spotless
    
    * Apply spotless
    
    ---------
    
    Co-authored-by: Álex Ruiz <alejandro.ruiz.becerra@wazuh.com>

--- plugins/command-manager/src/main/java/com/wazuh/commandmanager/CommandManagerPlugin.java
+++ plugins/command-manager/src/main/java/com/wazuh/commandmanager/CommandManagerPlugin.java
@@ -12,16 +12,14 @@ import org.opensearch.client.Client;
 import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
 import org.opensearch.cluster.node.DiscoveryNodes;
 import org.opensearch.cluster.service.ClusterService;
-import org.opensearch.common.settings.ClusterSettings;
-import org.opensearch.common.settings.IndexScopedSettings;
-import org.opensearch.common.settings.Settings;
-import org.opensearch.common.settings.SettingsFilter;
+import org.opensearch.common.settings.*;
 import org.opensearch.core.common.io.stream.NamedWriteableRegistry;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.env.Environment;
 import org.opensearch.env.NodeEnvironment;
 import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.plugins.ReloadablePlugin;
 import org.opensearch.repositories.RepositoriesService;
 import org.opensearch.rest.RestController;
 import org.opensearch.rest.RestHandler;
@@ -30,6 +28,7 @@ import org.opensearch.threadpool.ThreadPool;
 import org.opensearch.watcher.ResourceWatcherService;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -37,21 +36,22 @@ import java.util.function.Supplier;
 
 import com.wazuh.commandmanager.index.CommandIndex;
 import com.wazuh.commandmanager.rest.RestPostCommandAction;
+import com.wazuh.commandmanager.settings.CommandManagerSettings;
 import com.wazuh.commandmanager.utils.httpclient.HttpRestClient;
-import com.wazuh.commandmanager.utils.httpclient.HttpRestClientDemo;
 
 /**
  * The Command Manager plugin exposes an HTTP API with a single endpoint to receive raw commands
  * from the Wazuh Server. These commands are processed, indexed and sent back to the Server for its
  * delivery to, in most cases, the Agents.
  */
-public class CommandManagerPlugin extends Plugin implements ActionPlugin {
+public class CommandManagerPlugin extends Plugin implements ActionPlugin, ReloadablePlugin {
     public static final String COMMAND_MANAGER_BASE_URI = "/_plugins/_command_manager";
     public static final String COMMANDS_URI = COMMAND_MANAGER_BASE_URI + "/commands";
     public static final String COMMAND_MANAGER_INDEX_NAME = ".commands";
     public static final String COMMAND_MANAGER_INDEX_TEMPLATE_NAME = "index-template-commands";
 
     private CommandIndex commandIndex;
+    private CommandManagerSettings commandManagerSettings;
 
     @Override
     public Collection<Object> createComponents(
@@ -68,10 +68,8 @@ public class CommandManagerPlugin extends Plugin implements ActionPlugin {
             Supplier<RepositoriesService> repositoriesServiceSupplier) {
         this.commandIndex = new CommandIndex(client, clusterService, threadPool);
 
-        // HttpRestClient stuff
-        String uri = "https://httpbin.org/post";
-        String payload = "{\"message\": \"Hello world!\"}";
-        HttpRestClientDemo.run(uri, payload);
+        this.commandManagerSettings = CommandManagerSettings.getInstance(environment);
+
         return Collections.emptyList();
     }
 
@@ -83,7 +81,26 @@ public class CommandManagerPlugin extends Plugin implements ActionPlugin {
             SettingsFilter settingsFilter,
             IndexNameExpressionResolver indexNameExpressionResolver,
             Supplier<DiscoveryNodes> nodesInCluster) {
-        return Collections.singletonList(new RestPostCommandAction(this.commandIndex));
+        return Collections.singletonList(
+                new RestPostCommandAction(this.commandIndex, this.commandManagerSettings));
+    }
+
+    @Override
+    public List<Setting<?>> getSettings() {
+        return Arrays.asList(
+                // Register API settings
+                CommandManagerSettings.M_API_AUTH_USERNAME,
+                CommandManagerSettings.M_API_AUTH_PASSWORD,
+                CommandManagerSettings.M_API_URI);
+    }
+
+    @Override
+    public void reload(Settings settings) {
+        // secure settings should be readable
+        // final CommandManagerSettings commandManagerSettings =
+        // CommandManagerSettings.getClientSettings(secureSettingsPassword);
+        // I don't know what I have to do when we want to reload the settings already
+        // xxxService.refreshAndClearCache(commandManagerSettings);
     }
 
     /**
