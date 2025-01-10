8f4ce34becd0f26e2e61aaf65639213ac5a4b549
Malena Casas <malena.casas@wazuh.com>
2025-01-10 12:15:06 -0300

    Retrieve the CommandManagerTest class

---
+++ plugins/command-manager/src/test/java/com/wazuh/commandmanager/CommandManagerTests.java
@@ -0,0 +1,8 @@
+package com.wazuh.commandmanager;
+import org.opensearch.test.OpenSearchTestCase;
+
+public class CommandManagerTests extends OpenSearchTestCase {
+    public void testPost_badUri() {}
+    public void testPost_badPayload() {}
+
+}
