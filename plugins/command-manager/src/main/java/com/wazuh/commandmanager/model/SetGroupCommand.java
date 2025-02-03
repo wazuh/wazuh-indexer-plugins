b6dd4440bcb48255e6599eb0f7e8ee34432c4167
Malena Casas <malena.casas@wazuh.com>
2025-02-03 09:57:42 -0300

    Add hard validation to set-group and fetch-config commands (#253)
    * Add dedicated models for set-group and fetch-config commands
    
    * Add ActionName Enum
    
    * Fix some errores in parse of set-group. Add the first test in SetGroupCommandTests
    
    * Add another four tests in SetGroupCommandTests
    
    * Change manage of Exceptions in Action class. Add test in FetchConfigCommandTests
    
    * Update openapi.yml
    
    Code clean up
    
    * Revert variable initialization to empty string
    
    * Remove log.warn added for testing
    
    ---------
    
    Co-authored-by: Alex Ruiz <alejandro.ruiz.becerra@wazuh.com>

---
+++ plugins/command-manager/src/main/java/com/wazuh/commandmanager/model/SetGroupCommand.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2024, Wazuh Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.wazuh.commandmanager.model;
+
+import org.opensearch.core.xcontent.XContentParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SetGroupCommand extends Args {
+
+    public static final String GROUPS_KEY = "groups";
+
+    /**
+     * Dedicated command.action.args parser for "set-group" action type.
+     *
+     * @param parser An XContentParser containing an args to be deserialized
+     * @return An Args object
+     * @throws IOException Rethrows the exception from list() and objectText() method
+     */
+    public static Args parse(XContentParser parser) throws IOException {
+        Map<String, Object> args = new HashMap<>();
+        List<String> groupList = new ArrayList<>();
+
+        // Parser currently on "args" key. Next expected token is START_OBJECT.
+        XContentParser.Token currentToken = parser.currentToken();
+        if (currentToken != XContentParser.Token.START_OBJECT) {
+            throw new IllegalArgumentException(
+                    "Expected [command.action.args] to be an object, got ["
+                            + parser.currentName()
+                            + "]");
+        }
+        // Next expected token is "groups" key, followed by an array of strings only.
+        currentToken = parser.nextToken();
+        if (currentToken != XContentParser.Token.FIELD_NAME) {
+            throw new IllegalArgumentException(
+                    "Expected [command.action.args] to be an object, got ["
+                            + currentToken.name()
+                            + "]");
+        }
+        if (!GROUPS_KEY.equals(parser.currentName())) {
+            throw new IllegalArgumentException(
+                    "Expected [command.action.args] to contain the [groups] key, got ["
+                            + parser.currentName()
+                            + "]");
+        }
+        // Next expected token is START_ARRAY.
+        currentToken = parser.nextToken();
+        if (currentToken != XContentParser.Token.START_ARRAY) {
+            throw new IllegalArgumentException(
+                    "Expected [command.action.args.groups] to be an array, got ["
+                            + currentToken.name()
+                            + "]");
+        }
+        // Iterate until token is END_ARRAY.
+        while (parser.nextToken() != XContentParser.Token.END_ARRAY) {
+            currentToken = parser.currentToken();
+
+            if (currentToken != XContentParser.Token.VALUE_STRING) {
+                throw new IllegalArgumentException(
+                        "Expected [command.action.args.groups] to be an array of strings only, got ["
+                                + currentToken.name()
+                                + "]");
+            }
+            groupList.add(parser.text());
+        }
+        // Consume the END_OBJECT token
+        parser.nextToken();
+
+        args.put(GROUPS_KEY, groupList);
+        return new Args(args);
+    }
+}
