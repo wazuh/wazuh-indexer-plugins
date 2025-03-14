a74528580ee4f73401589fa8165e4aeb3a38bca7
QU3B1M <kevin.ledesma@wazuh.com>
2025-03-13 22:52:38 -0300

    Add ContentUpdater Unit Tests

---
+++ plugins/content-manager/src/test/java/com/wazuh/contentmanager/updater/ContentUpdaterTests.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2024, Wazuh Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.wazuh.contentmanager.updater;
+
+import org.opensearch.client.Client;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.test.OpenSearchIntegTestCase;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import com.wazuh.contentmanager.client.CTIClient;
+import com.wazuh.contentmanager.client.CommandManagerClient;
+import com.wazuh.contentmanager.model.ctiapi.Offsets;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+@OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE)
+public class ContentUpdaterTests extends OpenSearchIntegTestCase {
+    private CTIClient mockCtiClient;
+    private CommandManagerClient mockCommandManagerClient;
+    private Client client;
+    private ClusterService clusterService;
+    private ContentUpdater contentUpdater;
+
+    @Before
+    public void setup() throws Exception {
+        super.setUp();
+        contentUpdater = new ContentUpdater();
+    }
+
+    public void testFetchAndApplyUpdates_NoNewUpdates() throws IOException {
+        ContentUpdater contentUpdaterSpy = Mockito.spy(contentUpdater);
+        doReturn(100L).when(contentUpdaterSpy).getCurrentOffset();
+        doReturn(100L).when(contentUpdaterSpy).getLatestOffset();
+
+        contentUpdaterSpy.fetchAndApplyUpdates();
+
+        verify(contentUpdaterSpy, never()).patchAndPostCommand(any());
+    }
+
+    public void testFetchAndApplyUpdates_WithNewUpdates() throws IOException {
+        ContentUpdater contentUpdaterSpy = Mockito.spy(contentUpdater);
+        doReturn(100L).when(contentUpdaterSpy).getCurrentOffset();
+        doReturn(102L).when(contentUpdaterSpy).getLatestOffset();
+        doReturn(new Offsets(Collections.emptyList()))
+                .when(contentUpdaterSpy)
+                .getContextChanges(anyString(), anyString());
+
+        contentUpdaterSpy.fetchAndApplyUpdates();
+
+        verify(contentUpdaterSpy).patchAndPostCommand(any());
+    }
+
+    public void testFetchAndApplyUpdates_ApiFailure() throws IOException {
+        ContentUpdater contentUpdaterSpy = Mockito.spy(contentUpdater);
+        doReturn(100L).when(contentUpdaterSpy).getCurrentOffset();
+        doReturn(102L).when(contentUpdaterSpy).getLatestOffset();
+        doThrow(new IOException("API error"))
+                .when(contentUpdaterSpy)
+                .getContextChanges(anyString(), anyString());
+
+        contentUpdaterSpy.fetchAndApplyUpdates();
+
+        verify(contentUpdaterSpy, never()).patchAndPostCommand(any());
+    }
+}
