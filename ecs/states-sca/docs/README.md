54e8b55fa1757375ab689090a1aa0171689667d3
abbonno <abbogonluq@gmail.com>
2025-04-02 12:10:11 +0200

    Fix states-sca configuration

--- ecs/states-sca/docs/README.md
+++ ecs/states-sca/docs/README.md
@@ -1,9 +1,7 @@
-## `wazuh-states-inventory-system` index data model
+## `wazuh-states-sca` index data model
 
 ### Fields summary
 
 The fields are based on https://github.com/wazuh/wazuh-indexer-plugins/issues/351#issue-2956934075
 
-Based on ECS:
-
-The detail of the fields can be found in csv file [Inventory System Fields](fields.csv).
+The detail of the fields can be found in csv file [SCA Fields](fields.csv).
--- ecs/states-sca/event-generator/event_generator.py
+++ ecs/states-sca/event-generator/event_generator.py
@@ -12,7 +12,7 @@ LOG_FILE = 'generate_data.log'
 GENERATED_DATA_FILE = 'generatedData.json'
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 # Default values
-INDEX_NAME = "wazuh-states-inventory-system"
+INDEX_NAME = "wazuh-states-sca"
 USERNAME = "admin"
 PASSWORD = "admin"
 IP = "127.0.0.1"
@@ -47,93 +47,90 @@ def generate_random_agent():
 def generate_random_host(is_root_level=False):
     if is_root_level:
         host = {
-                'architecture': random.choice(['x86_64', 'arm64']),
-                'hostname': f'host{random.randint(0, 1000)}',
-                'os': {
-                    'full': f'{random.choice(["debian", "ubuntu", "macos", "ios", "android", "RHEL"])} {random.randint(0, 99)}.{random.randint(0, 99)}',
-                    'kernel': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}',
-                    'name': random.choice(['Linux', 'Windows', 'macOS']),
-                    'platform': random.choice(['platform1', 'platform2']),
-                    'type': random.choice(['os_type1', 'os_type2']),
-                    'version': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}'
-                }
-            }
-    else:
-        host = {
             'architecture': random.choice(['x86_64', 'arm64']),
-            'boot': {
-                'id': f'bootid{random.randint(0, 9999)}'
-            },
-            'cpu': {
-                'usage': random.uniform(0, 100)
-            },
-            'disk': {
-                'read': {
-                    'bytes': random.randint(1000, 1000000)
-                },
-                'write': {
-                    'bytes': random.randint(1000, 1000000)
-                }
-            },
-            'domain': f'domain{random.randint(0, 1000)}',
-            'geo': generate_random_geo(),
             'hostname': f'host{random.randint(0, 1000)}',
-            'id': f'id{random.randint(0, 1000)}',
-            'ip': f'{random.randint(1, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}',
-            'mac': f'{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}',
-            'name': f'host{random.randint(0, 1000)}',
-            'network': {
-                'egress': {
-                    'bytes': random.randint(1000, 1000000),
-                    'packets': random.randint(100, 10000)
-                },
-                'ingress': {
-                    'bytes': random.randint(1000, 1000000),
-                    'packets': random.randint(100, 10000)
-                }
-            },
             'os': {
-                'family': random.choice(['debian', 'ubuntu', 'macos', 'ios', 'android', 'RHEL']),
                 'full': f'{random.choice(["debian", "ubuntu", "macos", "ios", "android", "RHEL"])} {random.randint(0, 99)}.{random.randint(0, 99)}',
                 'kernel': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}',
                 'name': random.choice(['Linux', 'Windows', 'macOS']),
                 'platform': random.choice(['platform1', 'platform2']),
                 'type': random.choice(['os_type1', 'os_type2']),
                 'version': f'{random.randint(0, 9)}.{random.randint(0, 9)}.{random.randint(0, 9)}'
+            }
+        }
+    else:
+        family = random.choice(
+            ['debian', 'ubuntu', 'macos', 'ios', 'android', 'RHEL'])
+        version = f'{random.randint(0, 99)}.{random.randint(0, 99)}'
+        host = {
+            'architecture': random.choice(['x86_64', 'arm64']),
+            'boot': {
+                'id': f'boot{random.randint(0, 9999)}'
             },
-            'pid_ns_ino': f'pid_ns{random.randint(0, 9999)}',
+            'cpu': {
+                'usage': random.uniform(0, 100)
+            },
+            'disk': {
+                'read': {
+                    'bytes': random.randint(0, 1000000)
+                },
+                'write': {
+                    'bytes': random.randint(0, 1000000)
+                }
+            },
+            'domain': f'domain{random.randint(0, 999)}',
+            'geo': {
+                'city_name': random.choice(['San Francisco', 'New York', 'Berlin', 'Tokyo']),
+                'continent_code': random.choice(['NA', 'EU', 'AS']),
+                'continent_name': random.choice(['North America', 'Europe', 'Asia']),
+                'country_iso_code': random.choice(['US', 'DE', 'JP']),
+                'country_name': random.choice(['United States', 'Germany', 'Japan']),
+                'location': {
+                    'lat': round(random.uniform(-90.0, 90.0), 6),
+                    'lon': round(random.uniform(-180.0, 180.0), 6)
+                },
+                'name': f'geo{random.randint(0, 999)}',
+                'postal_code': f'{random.randint(10000, 99999)}',
+                'region_iso_code': f'region{random.randint(0, 999)}',
+                'region_name': f'Region {random.randint(0, 999)}',
+                'timezone': random.choice(['PST', 'EST', 'CET', 'JST'])
+            },
+            'hostname': f'host{random.randint(0, 9999)}',
+            'id': f'hostid{random.randint(0, 9999)}',
+            'ip': f'{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}',
+            'mac': f'{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}:{random.randint(0, 255):02x}',
+            'name': f'hostname{random.randint(0, 9999)}',
+            'network': {
+                'egress': {
+                  'bytes': random.randint(0, 1000000),
+                  'packets': random.randint(0, 1000000)
+                },
+                'ingress': {
+                    'bytes': random.randint(0, 1000000),
+                    'packets': random.randint(0, 1000000)
+                }
+            },
+            'os': {
+                'family': family,
+                'full': f'{family} {version}',
+                'kernel': f'kernel{random.randint(0, 999)}',
+                'name': family,
+                'platform': random.choice(['linux', 'windows', 'macos']),
+                'type': family,
+                'version': version
+            },
+            'pid_ns_ino': f'{random.randint(1000000, 9999999)}',
             'risk': {
                 'calculated_level': random.choice(['low', 'medium', 'high']),
-                'calculated_score': random.uniform(0, 10),
+                'calculated_score': random.uniform(0, 100),
                 'calculated_score_norm': random.uniform(0, 1),
                 'static_level': random.choice(['low', 'medium', 'high']),
-                'static_score': random.uniform(0, 10),
+                'static_score': random.uniform(0, 100),
                 'static_score_norm': random.uniform(0, 1)
             },
-            'type': random.choice(['type1', 'type2']),
-            'uptime': random.randint(1000, 1000000)
+            'uptime': random.randint(0, 1000000)
         }
     return host
-
-
-def generate_random_geo():
-    geo = {
-        'city_name': 'CityName',
-        'continent_code': 'NA',
-        'continent_name': 'North America',
-        'country_iso_code': 'US',
-        'country_name': 'United States',
-        'location': {
-            'lat': round(random.uniform(-90, 90), 6),
-            'lon': round(random.uniform(-180, 180), 6)
-        },
-        'name': f'location{random.randint(0, 999)}',
-        'postal_code': f'{random.randint(10000, 99999)}',
-        'region_iso_code': 'US-CA',
-        'region_name': 'California',
-        'timezone': 'America/Los_Angeles'
-    }
-    return geo
 
 def generate_random_policy():
     policy = {
@@ -162,13 +159,17 @@ def generate_random_check():
     return check
 
 def generate_random_data(number):
-    return [{
-        '@timestamp': generate_random_date(),
-        'agent': generate_random_agent(),
-        'host': generate_random_host(),
-        'policy': generate_random_policy(),
-        'check': generate_random_check()
-    } for _ in range(number)]
+    data = []
+    for _ in range(number):
+        event_data = {
+            '@timestamp': generate_random_date(),
+            'agent': generate_random_agent(),
+            'host': generate_random_host(),
+            'policy': generate_random_policy(),
+            'check': generate_random_check()
+        }
+        data.append(event_data)
+    return data
 
 def inject_events(ip, port, index, username, password, data):
     url = f'https://{ip}:{port}/{index}/_doc'
@@ -193,15 +194,23 @@ def main():
     except ValueError:
         logging.error("Invalid input. Please enter a valid number.")
         return
+
     logging.info(f"Generating {number} events...")
     data = generate_random_data(number)
+
     with open(GENERATED_DATA_FILE, 'a') as outfile:
         for event_data in data:
             json.dump(event_data, outfile)
             outfile.write('\n')
+
     logging.info('Data generation completed.')
     if input("Do you want to inject the generated data into your indexer? (y/n) ").strip().lower() == 'y':
-        inject_events(IP, PORT, INDEX_NAME, USERNAME, PASSWORD, data)
+        ip = input(f"Enter the IP of your Indexer (default: '{IP}'): ") or IP
+        port = input(f"Enter the port of your Indexer (default: '{PORT}'): ") or PORT
+        index = input(f"Enter the index name (default: '{INDEX_NAME}'): ") or INDEX_NAME
+        username = input(f"Username (default: '{USERNAME}'): ") or USERNAME
+        password = input(f"Password (default: '{PASSWORD}'): ") or PASSWORD
+        inject_events(ip, port, index, username, password, data)
 
 if __name__ == "__main__":
     main()
