3c03d8a6fbd37a71683d4fbeb2ebb61ad6a2aeea
Malena Casas <malena.casas@wazuh.com>
2025-04-03 18:25:33 -0300

    First design of solution

--- plugins/content-manager/src/main/java/com/wazuh/contentmanager/client/CTIClient.java
+++ plugins/content-manager/src/main/java/com/wazuh/contentmanager/client/CTIClient.java
@@ -34,7 +34,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.*;
 
 import com.wazuh.contentmanager.model.ctiapi.ConsumerInfo;
 import com.wazuh.contentmanager.settings.PluginSettings;
@@ -55,6 +55,10 @@ public class CTIClient extends HttpClient {
             "/catalog/contexts/vd_1.0.0/consumers/vd_4.8.0";
     private static final String CONSUMER_CHANGES_ENDPOINT = "/changes";
 
+    private static final int MAX_OFFSETS = 1000;
+    private static final int MAX_ATTEMPTS = 3;
+    private static final int SLEEP_TIME = 1000;
+
     /** Enum representing the query parameters used in CTI API requests. */
     public enum QueryParameters {
         /** The starting offset parameter TO_OFFSET - FROM_OFFSET must be >1001 */
@@ -67,6 +71,27 @@ public class CTIClient extends HttpClient {
         private final String value;
 
         QueryParameters(String value) {
+            this.value = value;
+        }
+
+        /**
+         * Returns the string representation of the query parameter.
+         *
+         * @return The query parameter key as a string.
+         */
+        public String getValue() {
+            return value;
+        }
+    }
+
+    /** Enum representing the query parameters used in CTI API requests. */
+    public enum Functions {
+        GET_CHANGES("getChanges"),
+        GET_CATALOG("getCatalog");
+
+        private final String value;
+
+        Functions(String value) {
             this.value = value;
         }
 
@@ -110,9 +135,25 @@ public class CTIClient extends HttpClient {
      * @param withEmpties A flag indicating whether to include empty values (Optional).
      * @return A {@link SimpleHttpResponse} containing the API response.
      */
-    public SimpleHttpResponse getChanges(String fromOffset, String toOffset, String withEmpties) {
+    public SimpleHttpResponse getChanges(String fromOffset, String toOffset, String withEmpties)
+            throws HttpException, IllegalArgumentException {
         Map<String, String> params = contextQueryParameters(fromOffset, toOffset, withEmpties);
-        return sendRequest(Method.GET, CONSUMER_CHANGES_ENDPOINT, null, params, (Header) null);
+        CompletableFuture<SimpleHttpResponse> futureResponse = new CompletableFuture<>();
+        fetchWithRetry(
+                Functions.GET_CHANGES,
+                Method.GET,
+                CONSUMER_CHANGES_ENDPOINT,
+                null,
+                params,
+                null,
+                MAX_ATTEMPTS,
+                futureResponse);
+
+        try {
+            return futureResponse.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new HttpException("Failed to fetch changes: " + e.getMessage(), e);
+        }
     }
 
     /**
@@ -151,14 +192,146 @@ public class CTIClient extends HttpClient {
      * @return A map containing the query parameters.
      */
     public static Map<String, String> contextQueryParameters(
-            String fromOffset, String toOffset, String withEmpties) {
+            String fromOffset, String toOffset, String withEmpties) throws IllegalArgumentException {
         Map<String, String> params = new HashMap<>();
+        if (fromOffset == null) {
+            throw new IllegalArgumentException("fromOffset cannot be null");
+        }
+        if (toOffset == null) {
+            throw new IllegalArgumentException("toOffset cannot be null");
+        }
+        Long longFromOffset = Long.parseLong(fromOffset);
+        Long longToOffset = Long.parseLong(toOffset);
+        long difference = longToOffset - longFromOffset;
+        if (longToOffset < longFromOffset || difference > MAX_OFFSETS) {
+            throw new IllegalArgumentException(
+                    "toOffset cannot be less than fromOffset or the difference cannot be greater than "
+                            + MAX_OFFSETS);
+        }
+
         params.put(QueryParameters.FROM_OFFSET.getValue(), fromOffset);
         params.put(QueryParameters.TO_OFFSET.getValue(), toOffset);
         if (withEmpties != null && !withEmpties.isEmpty()) {
             params.put(QueryParameters.WITH_EMPTIES.getValue(), withEmpties);
         }
         return params;
+    }
+
+    /**
+     * Send a request to the CTI API and handles the HTTP response based on the provided status code.
+     *
+     * @param function The function to be executed.
+     * @param method The HTTP method to use for the request.
+     * @param endpoint The endpoint to append to the base API URI.
+     * @param body The request body (optional, applicable for POST/PUT).
+     * @param params The query parameters (optional).
+     * @param header The headers to include in the request (optional).
+     * @param attemptsLeft The number of remaining attempts.
+     * @param futureResponse The CompletableFuture to complete with the response.
+     * @throws IOException If an error occurs during response processing.
+     */
+    private void fetchWithRetry(
+            Functions function,
+            Method method,
+            String endpoint,
+            String body,
+            Map<String, String> params,
+            Header header,
+            int attemptsLeft,
+            CompletableFuture<SimpleHttpResponse> futureResponse) {
+        SimpleHttpResponse response = sendRequest(method, endpoint, body, params, header);
+
+        int statusCode = response.getCode();
+        Boolean retry = false;
+
+        switch (statusCode) {
+            case 200:
+                log.info("Operation succeeded: Status code 200");
+                futureResponse.complete(response);
+                break;
+            case 400:
+                futureResponse.completeExceptionally(
+                        new HttpException(
+                                "Operation failed: Status code 400 - Error: " + response.getBodyText()));
+                break;
+            case 429:
+                if (attemptsLeft > 0) {
+                    log.warn("Too many requests: Status code 429 - Error: {}", response.getBodyText());
+                    retry = true;
+                } else {
+                    futureResponse.completeExceptionally(
+                            new HttpException(
+                                    "Too many requests: Status code 429 - Error: " + response.getBodyText()));
+                }
+                break;
+            case 422:
+                if (attemptsLeft > 0) {
+                    log.warn("Unprocessable Entity: Status code 422 - Error: {}", response.getBodyText());
+                    retry = true;
+                } else {
+                    futureResponse.completeExceptionally(
+                            new HttpException(
+                                    "Server Error: Status code 500 - Error: " + response.getBodyText()));
+                }
+                break;
+            case 500:
+                if (attemptsLeft > 0) {
+                    log.error("Server Error: Status code 500 - Error: {}", response.getBodyText());
+                    retry = true;
+                } else {
+                    futureResponse.completeExceptionally(
+                            new HttpException(
+                                    "Server Error: Status code 500 - Error: " + response.getBodyText()));
+                }
+                break;
+            default:
+                log.error("Unexpected status code: {}", statusCode);
+                futureResponse.completeExceptionally(
+                        new HttpException(
+                                "Unexpected status code: " + statusCode + " - Error: " + response.getBodyText()));
+                break;
+        }
+
+        if (retry) {
+            ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+            int timeout = (MAX_ATTEMPTS - attemptsLeft + 1) * SLEEP_TIME;
+            switch (function) {
+                case GET_CHANGES:
+                    scheduler.schedule(
+                            () ->
+                                    fetchWithRetry(
+                                            Functions.GET_CHANGES,
+                                            Method.GET,
+                                            CONSUMER_CHANGES_ENDPOINT,
+                                            null,
+                                            params,
+                                            null,
+                                            attemptsLeft,
+                                            futureResponse),
+                            timeout,
+                            TimeUnit.MILLISECONDS);
+                    scheduler.shutdown();
+                    break;
+                case GET_CATALOG:
+                    scheduler.schedule(
+                            () ->
+                                    fetchWithRetry(
+                                            Functions.GET_CATALOG,
+                                            Method.GET,
+                                            CONSUMER_INFO_ENDPOINT,
+                                            null,
+                                            null,
+                                            null,
+                                            attemptsLeft,
+                                            futureResponse),
+                            timeout,
+                            TimeUnit.MILLISECONDS);
+                    scheduler.shutdown();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     /***
@@ -205,5 +378,15 @@ public class CTIClient extends HttpClient {
         } catch (InterruptedException e) {
             log.error("Snapshot download was interrupted: {}", e.getMessage());
         }
+    }
+
+    /**
+     * Handles the HTTP response based on the provided status code.
+     *
+     * @param response The HTTP response to be handled.
+     * @throws IOException If an error occurs during response processing.
+     */
+    private void handleHttpResponse(SimpleHttpResponse response) throws IOException {
+        int statusCode = response.getCode();
     }
 }
