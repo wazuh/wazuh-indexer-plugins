65c56f136b26d9b23dace57c51982e024108d6f4
f-galland <federico.galland@wazuh.com>
2025-04-23 08:57:39 -0300

    Apply spotless

--- plugins/content-manager/src/main/java/com/wazuh/contentmanager/utils/SnapshotHelper.java
+++ plugins/content-manager/src/main/java/com/wazuh/contentmanager/utils/SnapshotHelper.java
@@ -122,11 +122,12 @@ public class SnapshotHelper {
     /** Posts a command to the command manager API on a successful snapshot operation */
     @VisibleForTesting
     void postUpdateCommand() {
-        Privileged.doPrivilegedRequest(() ->
-            { CommandManagerClient.getInstance()
-                .postCommand(Command.create(String.valueOf(this.contextIndex.getLastOffset())));
-                return null;
-            });
+        Privileged.doPrivilegedRequest(
+                () -> {
+                    CommandManagerClient.getInstance()
+                            .postCommand(Command.create(String.valueOf(this.contextIndex.getLastOffset())));
+                    return null;
+                });
     }
 
     /**
