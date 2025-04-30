742ac9fc42ae54a0d1d0cfc5c72d36515dc4f5c4
QU3B1M <kevin.ledesma@wazuh.com>
2025-04-30 09:21:10 -0300

    Implement minimal required functionality to ContentUpdaterJobRunner

--- plugins/content-manager/src/main/java/com/wazuh/contentmanager/jobscheduler/ContentUpdaterJobRunner.java
+++ plugins/content-manager/src/main/java/com/wazuh/contentmanager/jobscheduler/ContentUpdaterJobRunner.java
@@ -16,12 +16,76 @@
  */
 package com.wazuh.contentmanager.jobscheduler;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.client.Client;
 import org.opensearch.jobscheduler.spi.JobExecutionContext;
 import org.opensearch.jobscheduler.spi.ScheduledJobParameter;
 import org.opensearch.jobscheduler.spi.ScheduledJobRunner;
+import org.opensearch.threadpool.ThreadPool;
 
 public class ContentUpdaterJobRunner implements ScheduledJobRunner {
+    private static final Logger log = LogManager.getLogger(ContentUpdaterJobRunner.class);
+
+    /** Singleton instance. */
+    private static ContentUpdaterJobRunner INSTANCE;
+
+    private Client client;
+    private ThreadPool threadPool;
+
+    /** Private default constructor. */
+    private ContentUpdaterJobRunner() {}
+
+    /**
+     * Default Singleton instance access method.
+     *
+     * @return the singleton instance.
+     */
+    public static ContentUpdaterJobRunner getInstance() {
+        if (ContentUpdaterJobRunner.INSTANCE == null) {
+            INSTANCE = new ContentUpdaterJobRunner();
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Singleton instance access method with parameters.
+     *
+     * @param client OpenSearch's client.
+     * @param threadPool OpenSearch's thread pool.
+     * @return the singleton instance.
+     */
+    public static ContentUpdaterJobRunner getInstance(Client client, ThreadPool threadPool) {
+        if (ContentUpdaterJobRunner.INSTANCE == null) {
+            INSTANCE = new ContentUpdaterJobRunner();
+        }
+        INSTANCE.setClient(client);
+        INSTANCE.setThreadPool(threadPool);
+        return INSTANCE;
+    }
+
     @Override
     public void runJob(
-            ScheduledJobParameter scheduledJobParameter, JobExecutionContext jobExecutionContext) {}
+            ScheduledJobParameter scheduledJobParameter, JobExecutionContext jobExecutionContext) {
+        ContentUpdaterRunnable jobRunnable = new ContentUpdaterRunnable(this.client);
+        this.threadPool.generic().submit(jobRunnable);
+    }
+
+    /**
+     * Sets the client.
+     *
+     * @param client OpenSearch's client.
+     */
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    /**
+     * Sets the thread pool.
+     *
+     * @param threadPool OpenSearch's thread pool.
+     */
+    public void setThreadPool(ThreadPool threadPool) {
+        this.threadPool = threadPool;
+    }
 }
