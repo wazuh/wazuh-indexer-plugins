2301bbe698de29eb71035a87d5547fcc19f8007a
Jorge Sánchez <jorge.sanchez@wazuh.com>
2025-05-28 11:52:51 +0200

    Adapt command manager plugin for OpenSearch 3.0.0 (#456)
    * Adapt imports and dependencies
    
    * Update CHANGELOG.md
    
    Signed-off-by: Jorge Sánchez <jorge.sanchez@wazuh.com>
    
    ---------
    
    Signed-off-by: Jorge Sánchez <jorge.sanchez@wazuh.com>
    Signed-off-by: Álex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>
    Co-authored-by: Álex Ruiz Becerra <alejandro.ruiz.becerra@wazuh.com>

--- plugins/command-manager/src/main/java/com/wazuh/commandmanager/utils/Search.java
+++ plugins/command-manager/src/main/java/com/wazuh/commandmanager/utils/Search.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
-import org.opensearch.client.support.AbstractClient;
+import org.opensearch.transport.client.support.AbstractClient;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.index.query.BoolQueryBuilder;
 import org.opensearch.index.query.QueryBuilders;
