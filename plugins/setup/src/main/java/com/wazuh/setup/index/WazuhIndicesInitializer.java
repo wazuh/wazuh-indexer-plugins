2a137e92fa66eb6d04d9bdbd320b201183dc374f
f-galland <federico.galland@wazuh.com>
2025-06-13 11:56:48 -0300

    Remove warning suppression

--- plugins/setup/src/main/java/com/wazuh/setup/index/WazuhIndicesInitializer.java
+++ plugins/setup/src/main/java/com/wazuh/setup/index/WazuhIndicesInitializer.java
@@ -93,7 +93,6 @@ public final class WazuhIndicesInitializer implements IndexInitializer {
      *
      * @param indexStrategySelector: The IndexStrategySelector object to load
      */
-    @SuppressWarnings("unchecked")
     private void putTemplate(IndexStrategySelector indexStrategySelector) {
         try {
             Map<String, Object> template =
