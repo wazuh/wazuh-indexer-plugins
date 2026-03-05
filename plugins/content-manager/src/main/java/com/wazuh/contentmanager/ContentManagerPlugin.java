e56427343b4712c5d859250de4fa8f7236fd572f
f-galland <federico.galland@wazuh.com>
2026-03-05 14:48:23 -0300

    Pass engineService to CatalogSyncJob()

--- plugins/content-manager/src/main/java/com/wazuh/contentmanager/ContentManagerPlugin.java
+++ plugins/content-manager/src/main/java/com/wazuh/contentmanager/ContentManagerPlugin.java
@@ -121,7 +121,7 @@ public class ContentManagerPlugin extends Plugin
 
         // Initialize CatalogSyncJob
         this.catalogSyncJob =
-                new CatalogSyncJob(this.client, this.consumersIndex, environment, this.threadPool);
+                new CatalogSyncJob(this.client, this.consumersIndex, environment, this.threadPool, this.engine);
 
         // Register Executors
         runner.registerExecutor(CatalogSyncJob.JOB_TYPE, this.catalogSyncJob);
