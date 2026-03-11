f4520847ba7c6a170e73dd008b78de6f5726908e
Adrian Ponce <adrian.ponce@wazuh.com>
2026-03-11 11:07:06 -0300

    Remove hashes from CVE per document and global

--- plugins/content-manager/src/main/java/com/wazuh/contentmanager/cti/catalog/model/Cve.java
+++ plugins/content-manager/src/main/java/com/wazuh/contentmanager/cti/catalog/model/Cve.java
@@ -38,10 +38,6 @@ public class Cve {
     @JsonProperty(KEY_PAYLOAD)
     private JsonNode payload;
 
-    // TODO - check if we would like to keep it.
-    //    @JsonProperty(Constants.KEY_HASH)
-    //    private CveHash hash;
-
     @JsonProperty(Constants.KEY_OFFSET)
     private Long offset;
 
@@ -56,8 +52,7 @@ public class Cve {
      * violate the strict index mapping.
      *
      * @param payload The raw JSON object containing the CVE data.
-     * @return A fully populated Cve instance. //TODO: update return object 'with computed hash' if we
-     *     keep it.
+     * @return A fully populated Cve instance.
      */
     public static Cve fromPayload(JsonNode payload) {
         Cve cve = new Cve();
@@ -72,8 +67,7 @@ public class Cve {
         }
 
         cve.setPayload(payload);
-        // TODO: check if we want to keep it
-        // cve.setHash(new CveHash(Resource.computeSha256(payload.toString())));
+
         return cve;
     }
 
@@ -95,25 +89,6 @@ public class Cve {
         this.payload = payload;
     }
 
-    // TODO: check if we want to keep it
-    //    /**
-    //     * Gets the hash.
-    //     *
-    //     * @return The CVE hash.
-    //     */
-    //    public CveHash getHash() {
-    //        return this.hash;
-    //    }
-    //
-    //    /**
-    //     * Sets the hash.
-    //     *
-    //     * @param hash The CVE hash.
-    //     */
-    //    public void setHash(CveHash hash) {
-    //        this.hash = hash;
-    //    }
-
     /**
      * Gets the CTI offset.
      *
@@ -132,42 +107,4 @@ public class Cve {
         this.offset = offset;
     }
 
-    // TODO: check if we want to keep it
-    //    /** Represents the {@code hash} object within a CVE, containing a SHA-256 checksum. */
-    //    @JsonInclude(JsonInclude.Include.NON_NULL)
-    //    public static class CveHash {
-    //
-    //        @JsonProperty(Constants.KEY_SHA256)
-    //        private String sha256;
-    //
-    //        /** Default constructor. */
-    //        public CveHash() {}
-    //
-    //        /**
-    //         * Creates a CveHash with the given SHA-256 value.
-    //         *
-    //         * @param sha256 The SHA-256 hash string.
-    //         */
-    //        public CveHash(String sha256) {
-    //            this.sha256 = sha256;
-    //        }
-    //
-    //        /**
-    //         * Gets the SHA-256 hash.
-    //         *
-    //         * @return The SHA-256 hash string.
-    //         */
-    //        public String getSha256() {
-    //            return this.sha256;
-    //        }
-    //
-    //        /**
-    //         * Sets the SHA-256 hash.
-    //         *
-    //         * @param sha256 The SHA-256 hash string.
-    //         */
-    //        public void setSha256(String sha256) {
-    //            this.sha256 = sha256;
-    //        }
-    //    }
 }
