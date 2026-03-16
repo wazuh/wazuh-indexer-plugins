d1064b13a05f771fc6ed8ca218f4a40c21816186
QU3B1M <kevin.ledesma@wazuh.com>
2026-03-16 14:41:02 -0300

    Remove the usage of full class names

--- plugins/content-manager/src/test/java/com/wazuh/contentmanager/rest/service/RestPostFilterActionTests.java
+++ plugins/content-manager/src/test/java/com/wazuh/contentmanager/rest/service/RestPostFilterActionTests.java
@@ -160,7 +160,7 @@ public class RestPostFilterActionTests extends OpenSearchTestCase {
 
         // Mock SpaceService calculateAndUpdate to prevent NPE
         SpaceService spaceService = this.action.getPolicyHashService();
-        when(spaceService.calculateAndUpdate(any())).thenReturn(java.util.Collections.emptySet());
+        when(spaceService.calculateAndUpdate(any())).thenReturn(Collections.emptySet());
     }
 
     /**
