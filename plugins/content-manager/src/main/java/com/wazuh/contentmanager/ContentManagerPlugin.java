82af1c9b19893316589ecc6836184d13102da27d
Mauricio Fontebasso <mauricio.fontebasso@wazuh.com>
2026-09-21 12:49:47 +0200

    Fix missing import

--- plugins/content-manager/src/main/java/com/wazuh/contentmanager/ContentManagerPlugin.java
+++ plugins/content-manager/src/main/java/com/wazuh/contentmanager/ContentManagerPlugin.java
@@ -78,6 +78,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
